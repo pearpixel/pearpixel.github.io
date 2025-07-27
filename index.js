532c20299a7c7f516fff6cbb427b3a072dd60797
Mavellen <ZsDelegate@gmail.com>
2025-07-27 09:57:36 +0200

    base test

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const headeritems = [{text:"c/c++"}]
+function generate_header(element) 
+{
+  const header = document.createElement('header');
+  const title = document.createElement('h1');
+  title.textContent = "dendan";
+
+  for(let i = 0; i < headeritems.length; i++) {
+    const item = document.createElement('div');
+    item.textContent = headeritems[i].text;
+    header.appendChild(item);
+  }
+}
+
 const hwl = document.getElementById('root');
+generate_header(hwl);
 hwl.innerHTML = 'Hello world';
